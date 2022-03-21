b75a168a9110b64b909e4431252a2c5a434bc1cb
chou <choukatanagari@yahoo.com>
2022-03-21 17:43:37 +0800

    update

--- project/static/cropper.py
+++ project/static/cropper.py
@@ -8,11 +8,20 @@ img_files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 print(img_files)
 
-
+'''
+#cropping image
 for i in img_files:
     image = Image.open(mypath + i)
     cropper = image.crop((1200, 935, 1575, 1090))
     width, height = cropper.size
     cropper = cropper.resize(((width*3), (height)*3), Image.ANTIALIAS)
-    imageName = "project/static/geo/"+i
+    imageName = "project/static/thumbnails2/"+i
     cropper.save(imageName)
+'''
+#resize image
+for i in img_files:
+    image = Image.open(mypath + i)
+    size = 827,583
+    image = image.resize(size)
+    imageName = "project/static/thumbnails2/"+i
+    image.save(imageName)
