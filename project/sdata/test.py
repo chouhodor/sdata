eefd47ba1976f73b53d097e48237eb359c884877
chouhodor <choukatanagari@yahoo.com>
2022-03-21 13:06:40 +0800

    update

--- project/sdata/test.py
+++ project/sdata/test.py
@@ -1,17 +1,5 @@
-from os import listdir
-from os.path import isfile, join
-import os
+geo_list = ["pahang","kelantan","terengganu","johor","melaka","selangor",
+"kuala lumpur","negeri sembilan","perak","kedah","penang","perlis","putrajaya","sabah","sarawak",
+"labuan","tioman","langkawi"]
 
-#mypath = "C:\\stekapps\\project\\snake_data\\static\\snake_img\\1"
-
-mypath = 'project/static/snake_img/1'
-
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-
-print(onlyfiles)
-
-'''
-cwd = os.getcwd()
-
-print(cwd)
-'''
+print(sorted(geo_list))
